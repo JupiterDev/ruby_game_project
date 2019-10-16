f018d801a2248d5a791f611248607705ff366f1a
JupiterDev <ilyaklyus@gmail.com>
2019-10-17 01:29:41 +0300

    added messages module

--- control.rb
+++ control.rb
@@ -1,4 +1,10 @@
+require_relative 'messages'
+
 class Control
+  include Messages   # вынес все puts в отдельный модуль
+
   def start
+    greeting
+    ask_name
   end
 end
---
+++ messages.rb
@@ -0,0 +1,12 @@
+module Messages
+
+  def greeting
+    puts "Добро пожаловать в игру Black Jack."
+  end
+
+  def ask_name
+    puts "Введите ваше имя."
+  end
+  
+end
+
