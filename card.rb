96264378c67f0c6e2da0ff0594254c58177402ae
JupiterDev <ilyaklyus@gmail.com>
2019-10-22 04:14:31 +0300

    fixed card class

--- card.rb
+++ card.rb
@@ -1,9 +1,12 @@
 class Card
   attr_accessor :card_title, :card_suit, :card_worth
 
+  TITLES = %w[2 3 4 5 6 7 8 9 10 J Q K A]
+	SUITS = %w[♠ ♥ ♣ ♦]
+
   def initialize(title, suit)
-    @card_title = title
-    @card_suit = suit
+    @card_title = title if TITLES.include?(title)
+    @card_suit = suit if SUITS.include?(suit)
     @card_worth = 0
     count_worth(title)
   end
--- deck.rb
+++ deck.rb
@@ -2,9 +2,6 @@ require_relative 'card'
 
 class Deck
 	attr_accessor :card_set
-
-	CARD_TITLES = %w[2 3 4 5 6 7 8 9 10 J Q K A]
-	CARD_SUITS = %w[♠ ♥ ♣ ♦]
 	
 	def initialize
 		@card_set = []
@@ -12,8 +9,8 @@ class Deck
 	end
 
 	def add_cards_to_set
-		CARD_TITLES.each do |title|
-			CARD_SUITS.each do |suit|
+		Card::TITLES.each do |title|
+			Card::SUITS.each do |suit|
 				@card_set << Card.new(title, suit)
 			end
 		end
